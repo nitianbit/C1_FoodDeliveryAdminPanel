1001d12c4b8b2f49e34d5e1e398d3431dd481563
Tushar <tushar.mangla1120@gmail.com>
2024-02-25 20:44:27 +0530

    add IP Address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,8 @@ export const API_METHODS = {
 };
 
 // export const BASE_URL = "http://localhost:5400";
-export const BASE_URL = "https://delivery-bac.onrender.com"
+// export const BASE_URL = "https://delivery-bac.onrender.com"
+export const BASE_URL = "http://3.106.113.33:5400"
 export const CONTENT_TYPE_URL_ENCODED = "application/x-www-form-urlencoded"
 
 export const AUTH_ENDPOINTS = {
