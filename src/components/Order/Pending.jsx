458db350644ed1f6e5d00e293dfed84020aee641
Nitianbit <nitianbit@gmail.com>
2024-02-25 14:38:21 +0530

    "Ui changes"

--- src/components/Order/Pending.jsx
+++ src/components/Order/Pending.jsx
@@ -5,7 +5,7 @@ const Pending = ({ orderDetails, updateOrder, status }) => {
 
 
     return (
-        <div className='w-full grid md:grid-cols-1 lg:grid-cols-2  gap-4 overflow-y-auto'>
+        <div className='w-full grid lg:grid-cols-2 xl:grid-cols-3 overflow-y-auto gap-5'>
             {orderDetails?.map((item, indx) => {
                 return <OrderCard updateOrder={updateOrder} item={item} status={status} orderId={item?._id} />
             })}
--- src/pages/orders/SingleOrderDetail.jsx
+++ src/pages/orders/SingleOrderDetail.jsx
@@ -99,7 +99,7 @@ const SingleOrderDetail = ({ order }) => {
                     </div>
                     <div className="mt-10 flex flex-col xl:flex-row justify-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
                         <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
-                            <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
+                            <div className="flex flex-col justify-start items-start bg-gray-50 px-4 2 md:py-6 md:p-4 xl:p-4 w-full">
 
                                 <div className=" md:flex-row flex-col flex items-start justify-between w-full p-2 space-y-4 md:space-y-0">
                                     <div className="w-full flex flex-col justify-start items-start space-y-2">
@@ -111,42 +111,40 @@ const SingleOrderDetail = ({ order }) => {
                                         <p className="text-lg font-semibold leading-6 text-gray-800">Total</p>
                                     </div>
                                 </div>
-                                <div className="mt-4 justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-                                    {currentOrder?.items?.map((oitem, oIndex) => {
-                                        return (
-                                            <div key={oIndex} className="bg-white border-b border-gray-200 md:flex-row flex-col flex items-start justify-between w-full p-2 mb-4 space-y-4 md:space-y-0">
+                                <div className=" w-full bg-white">
+                                    {currentOrder?.items?.map((oitem, oIndex) => (
+                                        <div key={oIndex} className=" md:flex-row flex-col flex items-start justify-between w-full p-2 space-y-4 md:space-y-0">
                                                 <div className="w-full flex flex-col justify-start items-start space-y-2">
-                                                    <h3 className="text-lg font-semibold leading-6 text-gray-800">{oitem?.name ?? ""}</h3>
+                                                    <h3 className="text-lg font-semibold leading-6 text-gray-600">{oitem?.name ?? ""}</h3>
                                                 </div>
                                                 <div className="flex justify-between space-x-8 items-center w-full">
-                                                    <p className="text-lg leading-6">Rs {oitem?.price ?? 0}</p>
-                                                    <p className="text-lg leading-6 text-gray-800">{oitem?.quantity ?? 0}</p>
-                                                    <p className="text-lg font-semibold leading-6 text-gray-800">Rs {(oitem?.price ?? 0) * (oitem?.quantity ?? 0)} </p>
+                                                    <p className="text-lg leading-6 text-gray-600">{oitem?.price ?? 0}</p>
+                                                    <p className="text-lg leading-6 text-gray-600">{oitem?.quantity ?? 0}</p>
+                                                    <p className="text-lg leading-6 text-gray-600">{(oitem?.price ?? 0) * (oitem?.quantity ?? 0)} </p>
                                                 </div>
-                                            </div>
-
-                                        );
-                                    })}
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
 
-                            <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
-                                <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
-                                    <h3 className="text-xl  font-semibold leading-5 text-gray-800">Summary</h3>
-
-                                    <div className="flex justify-between items-center w-full">
-                                        <p className="text-base  font-semibold leading-4 text-gray-800">Total</p>
-                                        <p className="text-base  font-semibold leading-4 text-gray-600">{currentOrder?.totalAmount}</p>
+                            <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 ">
+                            <div className="flex justify-between items-center w-full px-5">
+                                        <p className="text-lg  font-semibold leading-4 text-gray-900">Total</p>
+                                        <p className="text-lg  font-semibold leading-4 text-gray-800">Rs {currentOrder?.totalAmount}</p>
                                     </div>
-                                </div>
 
                             </div>
                         </div>
-                        <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
-                            <div className='bg-white w-full my-5 space-y-3'>
-                                <h4 className="text-lg  font-semibold leading-5 text-gray-800">{currentOrder?.status === "Pending" ? `Select Driver` : 'Driver details'}</h4>
+                        <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-4 xl:p-4 flex-col">
+                            <div className=' w-full my-3 space-y-4'>
+                                <h4 className="text-base  font-semibold leading-5 text-gray-800 pb-4">{currentOrder?.status === "Pending" ? `Select Driver` : 'Driver details'}</h4>
+                                <span className='text-sm text-gray-600'>
                                 {currentOrder?.status === "Pending" ? <DriverSelect driverValue={currentOrder?.driverInfo} setDriverValue={setCurrentOrder} allDrivers={drivers} /> : `${currentOrder?.driverInfo?.name} ${currentOrder?.driverInfo?.mob_no}`}
-                                <h4 className="text-lg  font-semibold leading-5 text-gray-800">Delivery Time</h4>
+
+                                </span>
+                                <hr />
+                                <h4 className="text-base  font-semibold leading-5 text-gray-800 pb-4">Delivery Time</h4>
+                                <span className='text-sm text-gray-600 '>
                                 {currentOrder?.status === "Pending" ? <Input
                                     label='Estimated Delivery Time'
                                     type='text'
@@ -158,25 +156,26 @@ const SingleOrderDetail = ({ order }) => {
                                         }))
                                     }}
                                 /> : currentOrder?.deliveryTime}
-
+                                </span>
+                                
                             </div>
-                            <h3 className="text-xl  font-semibold leading-5 text-gray-800 mt-2">Customer</h3>
+                            <h3 className="text-base  font-semibold leading-5 text-gray-800 my-2">Customer</h3>
                             <div className="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
                                 <div className="flex flex-col justify-start items-start flex-shrink-0">
-                                    <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
+                                    <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-2 border-b border-gray-200">
                                         <div className="flex justify-start items-start flex-col space-y-2">
-                                            <p className="text-base  font-semibold leading-4 text-left text-gray-800">{currentOrder?.name}</p>
+                                            <p className="text-sm leading-4 text-left text-gray-800">{currentOrder?.name}</p>
                                         </div>
                                     </div>
 
-                                    <div className="flex justify-center text-gray-800  md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
+                                    <div className="flex justify-center text-gray-800  md:justify-start items-center space-x-4 py-2 border-b border-gray-200 w-full">
                                         <span>PhoneNo.</span>
                                         <p className="cursor-pointer text-sm leading-5 ">{currentOrder?.phoneNo}</p>
 
                                     </div>
-                                    <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
+                                    <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-2 md:mt-0">
                                         <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
-                                            <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
+                                            <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-4">
                                                 <p className="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">Shipping Address</p>
                                                 <p className="w-48 lg:w-full  xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">{currentOrder?.address}</p>
                                             </div>
